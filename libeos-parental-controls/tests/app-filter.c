318b35e3dac7b0a8b4dd6c3c97959eee64adf93d
Philip Withnall <withnall@endlessm.com>
2018-10-06 00:47:45 +0100

    libeos-parental-controls: Add placeholder unit tests
    This commit is mostly to put the test framework in place, and create a
    placeholder test library for libeos-parental-controls for packaging.
    
    Signed-off-by: Philip Withnall <withnall@endlessm.com>
    
    https://phabricator.endlessm.com/T23859

---
+++ libeos-parental-controls/tests/app-filter.c
@@ -0,0 +1,48 @@
+/* -*- mode: C; c-file-style: "gnu"; indent-tabs-mode: nil; -*-
+ *
+ * Copyright © 2018 Endless Mobile, Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * Authors:
+ *  - Philip Withnall <withnall@endlessm.com>
+ */
+
+#include "config.h"
+
+#include <glib.h>
+#include <gio/gio.h>
+#include <libeos-parental-controls/app-filter.h>
+#include <locale.h>
+
+
+/* A placeholder smoketest which checks that the error quark works. */
+static void
+test_app_filter_error_quark (void)
+{
+  g_assert_cmpint (epc_app_filter_error_quark (), !=, 0);
+}
+
+int
+main (int    argc,
+      char **argv)
+{
+  setlocale (LC_ALL, "");
+  g_test_init (&argc, &argv, NULL);
+
+  g_test_add_func ("/app-filter/error-quark", test_app_filter_error_quark);
+
+  return g_test_run ();
+}
