305129589e0c03186a240a3e95730d837f1333be
Philip Withnall <withnall@endlessm.com>
2020-01-15 12:13:25 +0000

    malcontent-client: Fix typo in use of flags
    This doesn’t actually change the behaviour, since the two types are
    equivalent.
    
    Signed-off-by: Philip Withnall <withnall@endlessm.com>

--- malcontent-client/malcontent-client.py
+++ malcontent-client/malcontent-client.py
@@ -66,9 +66,9 @@ def __set_app_filter(user_id, app_filter, interactive):
     If `interactive` is `True`, interactive polkit authorisation dialogues will
     be allowed. An exception will be raised on failure."""
     if interactive:
-        flags = Malcontent.GetAppFilterFlags.INTERACTIVE
+        flags = Malcontent.SetAppFilterFlags.INTERACTIVE
     else:
-        flags = Malcontent.GetAppFilterFlags.NONE
+        flags = Malcontent.SetAppFilterFlags.NONE
 
     connection = Gio.bus_get_sync(Gio.BusType.SYSTEM)
     manager = Malcontent.Manager.new(connection)
